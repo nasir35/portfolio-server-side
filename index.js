234fc507c1b9efdb8d396a4e06941533f77ac134
nasir35 <nasir2242001@gmail.com>
2025-07-10 23:36:54 +0600

    allowed all origin in cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const colors = require("colors");
+const cors = require("cors");
 
 const app = require("./app");
+app.use(cors());
 
 // database connection
 mongoose.connect(process.env.URI).then(() => {
