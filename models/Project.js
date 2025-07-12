bc85908a5d1421fe9c064d137d4270e22cb48588
nasir35 <nasir2242001@gmail.com>
2025-07-12 08:40:54 +0600

    modified schema properties

--- models/Project.js
+++ models/Project.js
@@ -1,40 +1,42 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const projectSchema = new mongoose.Schema({
+const projectSchema = new mongoose.Schema(
+  {
     projectName: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
-    description:{
-        type: String,
-        required: true
+    description: {
+      type: String,
+      required: true,
     },
     visitLink: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     githubLink: {
-        type: [String],
-        required: true
+      type: [String],
+      required: true,
     },
-    landingPageImage: {
-        type: String,
-        required: true
+    thumbnail: {
+      type: String,
+      required: true,
     },
-    images: {
-        type: [String],
-        required: true
+    gallery: {
+      type: [String],
+      required: true,
     },
     technologies: {
-        type: [String],
-        required: true
+      type: [String],
+      required: true,
     },
     note: {
-        type: String
-    }
-},
-{
-    timestamps: true
-});
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model('Project', projectSchema);
+module.exports = mongoose.model("Project", projectSchema);
