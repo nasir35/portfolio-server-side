4aae9f8f0178ac8db3c128313c88a41a4387d0b1
nasir35 <nasir2242001@gmail.com>
2025-07-12 07:39:58 +0600

    modified upload route

--- routes/upload.routes.js
+++ routes/upload.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const {uploadMultipleImages} = require("../controllers/upload.controller");
+const { uploadMultipleImages } = require("../controllers/upload.controller");
 const verifyToken = require("../middlewares/verifyToken");
 const router = express.Router();
 
-router.post("/upload", verifyToken, uploadMultipleImages);
+router.post("/", verifyToken, uploadMultipleImages);
 module.exports = router;
